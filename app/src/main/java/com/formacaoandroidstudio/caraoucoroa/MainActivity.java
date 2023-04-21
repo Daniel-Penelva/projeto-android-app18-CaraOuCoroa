129fe1d405468de0f8dc6dc8182e32270ec95da2
Daniel Penelva <d4n.andrade@gmail.com>
2023-04-21 02:35:30 -0300

    implementando app cara ou coroa - passar dados de um activity para outro

--- app/src/main/java/com/formacaoandroidstudio/caraoucoroa/MainActivity.java
+++ app/src/main/java/com/formacaoandroidstudio/caraoucoroa/MainActivity.java
@@ -2,14 +2,37 @@ package com.formacaoandroidstudio.caraoucoroa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button botaoJogar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        botaoJogar = findViewById(R.id.buttonJogar);
+
+        botaoJogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getApplicationContext(), ResultadoActivity.class);
+
+                /* Passar dados para outra tela */
+                int numero = new Random().nextInt(2);
+                intent.putExtra("numero", numero);
+
+                startActivity(intent);
+            }
+        });
     }
 }
 
@@ -19,5 +42,5 @@ public class MainActivity extends AppCompatActivity {
  * res > styles.xml > na tag style no atributo parent onde está 'DarkActionBar' vamos substituir pelo valor 'NoActionBar'.
  * Vale ressaltar que é possível criar a sua própria ActionBar.
  *
- *
+ * Vai ser adicionado um Listener diretamente no botão.
  * */
--- app/src/main/java/com/formacaoandroidstudio/caraoucoroa/ResultadoActivity.java
+++ app/src/main/java/com/formacaoandroidstudio/caraoucoroa/ResultadoActivity.java
@@ -2,13 +2,41 @@ package com.formacaoandroidstudio.caraoucoroa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class ResultadoActivity extends AppCompatActivity {
+
+    private ImageView imagemResultado;
+    private Button botaoVoltar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado);
+
+        imagemResultado = findViewById(R.id.imageMoeda);
+        botaoVoltar = findViewById(R.id.buttonVoltar);
+
+        // Recuperar Dados
+        Bundle dados = getIntent().getExtras();
+        int numero = dados.getInt("numero");
+
+        if(numero == 0){
+            imagemResultado.setImageResource(R.drawable.moeda_cara);
+        }else{
+            imagemResultado.setImageResource(R.drawable.moeda_coroa);
+        }
+
+        botaoVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Esse método encerra a activity atual e mostra a activity da última posição da pilha.
+                finish();
+            }
+        });
     }
 }
